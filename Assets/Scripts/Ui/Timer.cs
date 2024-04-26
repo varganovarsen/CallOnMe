f179624cae780a630d231a48a2f00be99b23deec
Arseniy Varganov <varganovarsen@gmail.com>
2024-04-26 09:05:33 +0300

    New timer

--- Assets/Scripts/Ui/Timer.cs
+++ Assets/Scripts/Ui/Timer.cs
@@ -1,14 +1,15 @@
+using StoryPoints;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Timer : MonoBehaviour 
+public class Timer : MonoBehaviour
 {
     public static Timer Instance;
 
-    public static event Action OnTimerRunOut;
 
     [SerializeField]
     float runOutTime;
@@ -18,35 +19,64 @@ public class Timer : MonoBehaviour
     [SerializeField]
     float startTime;
 
+    private StoryPointInvoker storyPointInvoker;
+
+    [SerializeField]
+    StoryPoint[] timeStoryPoints;
+    [SerializeField]
+    float[] timeStamps;
+
     static float _currentTime;
     static bool _started;
     public static bool TimeRunOut;
 
-    public static float CurrentTime  => _currentTime;
+    public static float CurrentTime => _currentTime;
     public static bool Started { get => _started; set => _started = value; }
-    public static float RunOutTime;
+    public float NextTimeStamp;
+    int _currentTimeStampConter = 0;
+
+
+
+    private void OnValidate()
+    {
+        storyPointInvoker = GetComponent<StoryPointInvoker>();
+        Array.Resize(ref timeStamps, timeStoryPoints.Length);
+    }
 
     private void Awake()
     {
         DontDestroyOnLoad(this);
         _started = true;
-        RunOutTime = runOutTime + startTime;
+
+        UpdateNextTimeStemp();
         _currentTime += startTime;
     }
+
+    private void UpdateNextTimeStemp() => NextTimeStamp = startTime + timeStamps[_currentTimeStampConter] / timeKoefficent;
+
 
     private void Update()
     {
         if (_started)
 
-        _currentTime += Time.deltaTime / timeKoefficent;
+            _currentTime += Time.deltaTime / timeKoefficent;
 
-        if (CurrentTime > RunOutTime && !TimeRunOut)
+        if (CurrentTime > NextTimeStamp && !TimeRunOut)
         {
-            OnTimerRunOut.Invoke();
-            TimeRunOut = true;
+            StoryPointInvoker.InvokeTimerStoryPoint(timeStoryPoints[_currentTimeStampConter]);
+            _currentTimeStampConter++;
+
+            if (timeStamps.Length >= _currentTimeStampConter)
+            {
+                TimeRunOut = true;
+                return;
+            }
+
+            UpdateNextTimeStemp();
         }
 
     }
+
 
 
 
